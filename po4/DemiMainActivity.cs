144eed792726daa87a9811467495d47998db3fed
Camphens <37102286+Camphens@users.noreply.github.com>
2018-05-31 14:46:43 +0200

    Revert "Extra Intent value deleted"
    This reverts commit e769b196865c5c04a306723e0c42cd53ba8985d9.

--- po4/DemiMainActivity.cs
+++ po4/DemiMainActivity.cs
@@ -70,6 +70,11 @@ namespace po4
 
         void Button_Click(object sender, EventArgs e)
         {
+            var intent = new Intent(this, typeof(DemiSecondActivity));
+
+            Toast.MakeText(this, "This is going to be a conflict ", ToastLength.Short).Show();
+
+
             var intent = new Intent(this, typeof(FabioActivity));
             StartActivity(intent);
         }
